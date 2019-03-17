f40afe00f3143b71941bd3b170889db10e00b5b3
Sathaporn Chumwong <mivstp3912@gmail.com>
2019-03-17 12:04:49 +0700

    add contac01

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-  items = ["Sathaporn","nana","papa","naja","naka","juju"];
-  constructor(public navCtrl: NavController) {
+ /* items = ["Sathaporn",
+          "nana",
+          "papa",
+          "naja","naka"
+  ,       "juju"]; */
 
+  items = [
+      {name:"Sathaporn",tel:"0891695082",avatar:"01.png"},
+      {name:"nana",tel:"08123456789",avatar:"02.png"},
+      {name:"naja",tel:"08212345678",avatar:"03.png"},
+      {name:"naha",tel:"08312345678",avatar:"04.png"},
+      {name:"naka",tel:"08412345678",avatar:"05.png"},
+  ];
+  itemSelected(item : String){
+    console.log("Selected Item: ",item);}
+  constructor(public navCtrl: NavController) {
+ 
   }
+
 
 }
